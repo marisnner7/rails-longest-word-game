39c1406b25abbdc1596852266b6e6f555514128e
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-21 20:26:37 -0300

    done logic for new gamesController

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,16 @@
 class GamesController < ApplicationController
+  #define a array with vowels
+  VOWELS = %w( A E I O U Y)
   def new
-
+    #chamar 5 letras de vogal aleatorias
+    @letters = Array.new(5) { VOWELS.sample }
+    #chamar 5 letras de a..z de forma aleatoria - vowels
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    #embaralhar!
+    @letters.shuffle!
   end
 
   def score
-
+    raise
   end
 end
